f1db73735675f3a2ef55dd772efd10b0cd589378
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-11-09 12:58:58 +0530

    Update site with express routing

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-http.createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    if(q.pathname === '/') {
-        console.log('ok');
-        fs.readFile('index.html', (err, data) => {
-            res.writeHead(200, {'Content-Type' : 'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    } else {
-        fileName = '.' + q.pathname + '.html';
-        fs.readFile(fileName, (err, data) => {
-            if (err) {
-                return fs.readFile('404.html', (err, data) => {
-                    res.writeHead(404, {'Content-Type': 'text/html'});
-                    res.write(data);
-                    return res.end();
-                });
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            return res.write(data);
-            return res.end();
-        });
-    }
-}).listen(8080)
+const PORT = 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
